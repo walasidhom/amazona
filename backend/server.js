49a18c0bba1edbe1a539d8093ff6030fa082e4ec
Wala SIDHOM <walasidhom@gmail.com>
2023-02-03 10:19:01 +0100

    third commit

--- backend/server.js
+++ backend/server.js
@@ -24,11 +24,7 @@ app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
+
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
@@ -38,6 +34,11 @@ app.get('/api/keys/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
 
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 //define the port
 const port = process.env.PORT || 5000;
 
